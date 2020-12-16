90f919cb6184610b4610132e418a4387a8946fe8
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-12-16 11:59:13 -0800

    did router.get

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,3 +2,12 @@ const router = require("express").Router();
 const fs = require('fs')
 const path = require("path");
 const db = path.join(__dirname, "../db/db.json");
+
+router.get("/note", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if(err) throw err;
+        
+        const newNotes = JSON.parse(data);
+        res.json(newNotes);
+    })
+})
