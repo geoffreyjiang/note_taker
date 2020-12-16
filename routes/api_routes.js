93edbab1630bea3d524a9e6a35cfd7b504378bd6
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-12-16 12:47:13 -0800

    started on routerpost

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
+const { json } = require("body-parser");
 const fs = require('fs')
 const path = require("path");
 const db = path.join(__dirname, "../db/db.json");
 
-router.get("/note", (req, res) => {
+router.get("/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         if(err) throw err;
         
         const newNotes = JSON.parse(data);
         res.json(newNotes);
+    })
+})
+
+router.post("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if(err) throw err
+        const newNotes = JSON.parse(data)
     })
 })
