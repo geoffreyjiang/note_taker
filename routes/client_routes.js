2b97f7daca69c915d79cf94f67b10d2292e59592
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-12-16 14:06:14 -0800

    clientroutes

---
+++ routes/client_routes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const path= require ("path");
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get("/assets/css/styles.css", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/assets/css/styles.css")); 
+ })
+
+ router.get("/js/index.js", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/assets/js/index.js")); 
+ })
+
+
+module.exports = router;
